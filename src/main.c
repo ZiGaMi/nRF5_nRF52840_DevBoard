ec58d220cb66bf7186e280c07f89deba02f1e3bd
Žiga Miklošič <ziga.miklosic@seltron.si>
2022-11-02 10:00:10 +0100

    - rename project name

--- src/main.c
+++ src/main.c
@@ -31,6 +31,19 @@
 // Periphery
 #include "systick.h"
 
+
+/*#include "nrf_drv_saadc.h"
+
+
+const nrf_drv_saadc_config_t m_saadc_config = {
+    .resolution = NRF_SAADC_RESOLUTION_12BIT,
+    .oversample = NRF_SAADC_OVERSAMPLE_DISABLED,
+    .interrupt_priority = 3,
+    .low_power_mode = true};
+
+*/
+
+
 ////////////////////////////////////////////////////////////////////////////////
 // Definitions
 ////////////////////////////////////////////////////////////////////////////////
@@ -46,6 +59,26 @@
 ////////////////////////////////////////////////////////////////////////////////
 // Functions
 ////////////////////////////////////////////////////////////////////////////////
+
+
+/*
+
+void saadc_callback(nrf_drv_saadc_evt_t const *p_event) 
+{
+  
+  if (p_event->type == NRF_DRV_SAADC_EVT_DONE) 
+  {
+        ret_code_t err_code;
+
+        //err_code = nrf_drv_saadc_buffer_convert(p_event->data.done.p_buffer, 1 );
+
+        APP_ERROR_CHECK(err_code);
+
+
+  }
+}
+*/
+
 
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -67,6 +100,28 @@ int main(void)
 
     // Init application
     app_init();
+
+
+
+/*
+    nrf_saadc_channel_config_t channel_bat_config = {
+      .resistor_p = NRF_SAADC_RESISTOR_DISABLED,
+      .resistor_n = NRF_SAADC_RESISTOR_DISABLED,
+      .gain = NRF_SAADC_GAIN1_4,
+      .reference = NRF_SAADC_REFERENCE_VDD4,
+      .acq_time = NRF_SAADC_ACQTIME_10US,
+      .mode = NRF_SAADC_MODE_SINGLE_ENDED,
+      .burst = NRF_SAADC_BURST_DISABLED,
+      .pin_p = (nrf_saadc_input_t)(NRF_SAADC_INPUT_AIN7),
+      .pin_n = (nrf_saadc_input_t)(NRF_SAADC_INPUT_DISABLED),
+    };
+
+    if ( NRF_SUCCESS != nrf_drv_saadc_init( &m_saadc_config, saadc_callback ))
+    { 
+        PROJECT_CONFIG_ASSERT( 0 );
+    }
+*/
+
 
 
     // Main loop
